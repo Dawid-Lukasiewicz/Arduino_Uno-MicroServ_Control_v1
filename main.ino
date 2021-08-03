43104d75e01fed2ee6cb26abf13f5e46bee5ed76
Dawid-Lukasiewicz <62220648+Dawid-Lukasiewicz@users.noreply.github.com>
2021-08-03 20:18:02 +0200

    Ready file

---
+++ main.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+
+Servo Serw;
+String Received = "";
+uint32_t Position = 0, Change = 5, DelayedTimeMs = 0;
+void setup()
+{
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
+    Serw.attach(3);
+}
+
+void loop()
+{
+    if (Serw.read() < 180)
+    {
+        if (Serial.available() > 0)
+        {
+            digitalWrite(13, HIGH);
+            Received = Serial.readStringUntil('\n');
+            while (Received == "stop" || Received == "")
+            {
+                Received = Serial.readStringUntil('\n');
+                if (Received == "start")
+                    break;
+            }
+        }
+        DelayedTimeMs += 2 * Change;
+        delay(DelayedTimeMs);
+        Serw.write(Position);
+        Position += Change;
+        digitalWrite(13, LOW);
+    }
+    else
+    {
+        DelayedTimeMs = 0;
+        Position = 0;
+        Serw.write(Position);
+        delay(100);
+    }
+}
